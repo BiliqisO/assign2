73ab5255bc6ebe8ae33de921c963ec2a5270091e
BiliqisO <biliqisonikoyi@gmail.com>
2023-09-29 09:30:40 +0100

    first commit

--- src/component/CreateCampaign.js
+++ src/component/CreateCampaign.js
@@ -6,178 +6,163 @@ import { supportedChains } from "../constants";
 import { parseEther } from "ethers";
 
 const CreateCampaign = () => {
-    let [isOpen, setIsOpen] = useState(false);
-    const [title, setTitle] = useState("");
-    const [goal, setGoal] = useState(0.5);
-    const [duration, setDuration] = useState(1);
-    const [sendingTx, setSendingTx] = useState(false);
-    const { connect, isActive, account, switchToChain } = useConnection();
+  let [isOpen, setIsOpen] = useState(false);
+  const [title, setTitle] = useState("");
+  const [goal, setGoal] = useState(0.5);
+  const [duration, setDuration] = useState(1);
+  const [sendingTx, setSendingTx] = useState(false);
+  const { connect, isActive, account, switchToChain } = useConnection();
 
-    const proposeCampaign = useProposeCampaign();
+  const proposeCampaign = useProposeCampaign();
 
-    function closeModal() {
-        setIsOpen(false);
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  const handleProposeCampaign = async () => {
+    if (!title || !goal || !duration) return alert("Please provide all valeus");
+    if (!isActive) return alert("please, connect");
+    try {
+      setSendingTx(true);
+      const tx = await proposeCampaign(
+        title,
+        parseEther(String(goal)),
+        duration * 60
+      );
+      const receipt = await tx.wait();
+      if (receipt.status === 0) return alert("tx failed");
+
+      alert("campaign created!!");
+      setIsOpen(false);
+    } catch (error) {
+      console.log("error: ", error);
+      if (error.info.error.code === 4001) {
+        return alert("You rejected the request");
+      }
+      alert("something went wrong");
+    } finally {
+      setSendingTx(false);
     }
+  };
+  return (
+    <Fragment>
+      <button
+        onClick={openModal}
+        className="w-[fit-content] block rounded-md mx-auto bg-blue-400 px-4 py-4 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        Create a Campaign
+      </button>
 
-    function openModal() {
-        setIsOpen(true);
-    }
-
-    const handleProposeCampaign = async () => {
-        if (!title || !goal || !duration)
-            return alert("Please provide all valeus");
-        if (!isActive) return alert("please, connect");
-        try {
-            setSendingTx(true);
-            const tx = await proposeCampaign(
-                title,
-                parseEther(String(goal)),
-                duration * 60
-            );
-            const receipt = await tx.wait();
-            if (receipt.status === 0) return alert("tx failed");
-
-            alert("campaign created!!");
-        } catch (error) {
-            console.log("error: ", error);
-            if (error.info.error.code === 4001) {
-                return alert("You rejected the request");
-            }
-            alert("something went wrong");
-        } finally {
-            setSendingTx(false);
-        }
-    };
-    return (
-        <Fragment>
-            <button
-                onClick={openModal}
-                className="w-[fit-content] block rounded-md mx-auto bg-blue-400 px-4 py-4 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-            >
-                Create a Campaign
-            </button>
-
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={closeModal}>
-                    <Transition.Child
-                        as={Fragment}
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                    >
-                        <div className="fixed inset-0 bg-black bg-opacity-50" />
-                    </Transition.Child>
-                    <div className="fixed inset-0 overflow-y-auto">
-                        <div className="flex min-h-full items-center justify-center p-4 text-center">
-                            <Transition.Child
-                                as={Fragment}
-                                enter="ease-out duration-300"
-                                enterFrom="opacity-0 scale-95"
-                                enterTo="opacity-100 scale-100"
-                                leave="ease-in duration-200"
-                                leaveFrom="opacity-100 scale-100"
-                                leaveTo="opacity-0 scale-95"
-                            >
-                                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
-                                    >
-                                        Create Campaign
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Lorem ipsum dolor sit amet
-                                            consectetur adipisicing elit.
-                                            Consequuntur, numquam.
-                                        </p>
-                                    </div>
-                                    <form className="mt-4 space-y-4">
-                                        <div className="flex flex-col">
-                                            <label className="font-bold">
-                                                Title
-                                            </label>
-                                            <input
-                                                value={title}
-                                                onChange={(e) =>
-                                                    setTitle(e.target.value)
-                                                }
-                                                type="text"
-                                                className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
-                                            />
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <label className="font-bold">
-                                                Goal (ETH Amount)
-                                            </label>
-                                            <input
-                                                value={goal}
-                                                onChange={(e) =>
-                                                    setGoal(
-                                                        Number(e.target.value)
-                                                    )
-                                                }
-                                                type="number"
-                                                className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
-                                            />
-                                        </div>
-                                        <div className="flex flex-col">
-                                            <label className="font-bold">
-                                                Duration(Minutes)
-                                            </label>
-                                            <input
-                                                value={duration}
-                                                onChange={(e) =>
-                                                    setDuration(
-                                                        Number(e.target.value)
-                                                    )
-                                                }
-                                                type="number"
-                                                className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
-                                            />
-                                        </div>
-
-                                        {isActive ? (
-                                            <div
-                                                onClick={handleProposeCampaign}
-                                                className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
-                                            >
-                                                {sendingTx
-                                                    ? "Creating Campaign..."
-                                                    : "Create Campaign"}
-                                            </div>
-                                        ) : account ? (
-                                            <div
-                                                onClick={() =>
-                                                    switchToChain(
-                                                        supportedChains[0]
-                                                    )
-                                                }
-                                                className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
-                                            >
-                                                Switch to Sepolia
-                                            </div>
-                                        ) : (
-                                            <div
-                                                onClick={connect}
-                                                disabled={sendingTx}
-                                                className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
-                                            >
-                                                Connect
-                                            </div>
-                                        )}
-                                    </form>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
+      <Transition
+        appear
+        show={isOpen}
+        as={Fragment}
+      >
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-50" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Create Campaign
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consequuntur, numquam.
+                    </p>
+                  </div>
+                  <form className="mt-4 space-y-4">
+                    <div className="flex flex-col">
+                      <label className="font-bold">Title</label>
+                      <input
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        type="text"
+                        className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
+                      />
                     </div>
-                </Dialog>
-            </Transition>
-        </Fragment>
-    );
+                    <div className="flex flex-col">
+                      <label className="font-bold">Goal (ETH Amount)</label>
+                      <input
+                        value={goal}
+                        onChange={(e) => setGoal(Number(e.target.value))}
+                        type="number"
+                        className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label className="font-bold">Duration(Minutes)</label>
+                      <input
+                        value={duration}
+                        onChange={(e) => setDuration(Number(e.target.value))}
+                        type="number"
+                        className="outline-0 py-2 px-1 rounded-lg mt-2 border border-blue-400"
+                      />
+                    </div>
+
+                    {isActive ? (
+                      <div
+                        onClick={handleProposeCampaign}
+                        className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
+                      >
+                        {sendingTx ? "Creating Campaign..." : "Create Campaign"}
+                      </div>
+                    ) : account ? (
+                      <div
+                        onClick={() => switchToChain(supportedChains[0])}
+                        className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
+                      >
+                        Switch to Sepolia
+                      </div>
+                    ) : (
+                      <div
+                        onClick={connect}
+                        disabled={sendingTx}
+                        className="cursor-pointer w-full rounded-md bg-blue-400 p-3 text-sm font-medium text-white hover:bg-opacity-90 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 text-center"
+                      >
+                        Connect
+                      </div>
+                    )}
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </Fragment>
+  );
 };
 
 export default CreateCampaign;
--- src/hooks/useAllCampaigns.js
+++ src/hooks/useAllCampaigns.js
@@ -2,63 +2,79 @@ import { useEffect, useState } from "react";
 import useCampaignCount from "./useCampaignCount";
 import { useConnection } from "../context/connection";
 import {
-    getCrowdfundContract,
-    getCrowdfundContractWithProvider,
+  getCrowdfundContract,
+  getCrowdfundContractWithProvider,
 } from "../utils";
 
 const useAllCampaigns = () => {
-    const [campaigns, setCampaigns] = useState([]);
-    const { provider } = useConnection();
-    const campaignNo = useCampaignCount();
+  const [campaigns, setCampaigns] = useState([]);
+  const { provider } = useConnection();
+  const campaignNo = useCampaignCount();
 
-    useEffect(() => {
-        const fetchAllCampaigns = async () => {
-            try {
-                const contract = await getCrowdfundContract(provider, false);
-                const campaignsKeys = Array.from(
-                    { length: Number(campaignNo) },
-                    (_, i) => i + 1
-                );
-                const campaignPromises = campaignsKeys.map((id) =>
-                    contract.crowd(id)
-                );
+  useEffect(() => {
+    const fetchAllCampaigns = async () => {
+      try {
+        const contract = await getCrowdfundContract(provider, false);
+        const campaignsKeys = Array.from(
+          { length: Number(campaignNo) },
+          (_, i) => i + 1
+        );
+        const campaignPromises = campaignsKeys.map((id) => contract.crowd(id));
+        const campaignContributors = campaignsKeys.map((id) =>
+          contract.getContributors(id)
+        );
+        //   const contributors = allContributors.toArray();
+        //   return contributors;
 
-                const campaignResults = await Promise.all(campaignPromises);
+        const campaignResults = await Promise.all(campaignPromises);
+        const contributorsResults = await Promise.all(campaignContributors);
 
-                const campaignDetails = campaignResults.map(
-                    (details, index) => ({
-                        id: campaignsKeys[index],
-                        title: details.title,
-                        fundingGoal: details.fundingGoal,
-                        owner: details.owner,
-                        durationTime: Number(details.durationTime),
-                        isActive: details.isActive,
-                        fundingBalance: details.fundingBalance,
-                        contributors: details.contributors,
-                    })
-                );
+        const campaignDetails = campaignResults.map((details, index) => ({
+          id: campaignsKeys[index],
+          title: details.title,
+          fundingGoal: details.fundingGoal,
+          owner: details.owner,
+          durationTime: Number(details.durationTime),
+          isActive: details.isActive,
+          fundingBalance: details.fundingBalance,
+          contributors: contributorsResults[index],
+        }));
 
-                setCampaigns(campaignDetails);
-            } catch (error) {
-                console.error("Error fetching campaigns:", error);
-            }
-        };
+        setCampaigns(campaignDetails);
+        console.log(campaignDetails);
+      } catch (error) {
+        console.error("Error fetching campaigns:", error);
+      }
+    };
 
-        fetchAllCampaigns();
+    fetchAllCampaigns();
+  }, [campaignNo, provider]);
 
-        // Listen for event
-        const handleProposeCampaignEvent = (id, title, amount, duration) => {
-            console.log({ id, title, amount, duration });
-        };
-        const contract = getCrowdfundContractWithProvider(provider);
-        contract.on("ProposeCampaign", handleProposeCampaignEvent);
+  useEffect(() => {
+    // Listen for event
+    const handleProposeCampaignEvent = (id, title, amount, duration) => {
+      console.log({ id, title, amount, duration });
+      const newCampaign = {
+        id: id,
+        title: title,
+        fundingGoal: amount,
+        durationTime: Number(duration),
+        isActive: true,
+        fundingBalance: 0,
+        contributors: [],
+      };
 
-        return () => {
-            contract.off("ProposeCampaign", handleProposeCampaignEvent);
-        };
-    }, [campaignNo, provider]);
+      setCampaigns((prevCampaigns) => [...prevCampaigns, newCampaign]);
+    };
+    const contract = getCrowdfundContractWithProvider(provider);
+    contract.on("ProposeCampaign", handleProposeCampaignEvent);
 
-    return campaigns;
+    return () => {
+      contract.off("ProposeCampaign", handleProposeCampaignEvent);
+    };
+  }, [provider]);
+
+  return campaigns;
 };
 
 export default useAllCampaigns;
